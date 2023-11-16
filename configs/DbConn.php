8dc6a2bdca6a574fd35f8d90f7922374d55bf4f7
emma <emmamuthoniii1@gmail.com>
2023-11-16 18:50:56 +0200

    configs

---
+++ configs/DbConn.php
@@ -0,0 +1,12 @@
+<?php
+
+
+ require_once "constants.php";
+    try{
+        $DbConn= new PDO("mysql:host=$hostname; dbname=$dbname", $username, $userpass);
+        $DbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected Successfully!! ";
+    } catch(PDOException $e){
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
